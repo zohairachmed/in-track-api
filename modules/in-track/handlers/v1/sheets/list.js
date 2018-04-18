7e3b8c07e8a17e3b9e8e2de2f521d8cf70f85db6
Zohair Ahmed <zohair.ahmed@charter.com>
2018-04-18 09:55:28 -0600

    Swagger document updated

---
+++ modules/in-track/handlers/v1/sheets/list.js
@@ -0,0 +1,20 @@
+'use strict';
+const SheetsRepo = require('../../../../data-store/sheets-repo');
+const debug = require('debug')('sheets/list');
+
+module.exports = {
+    get: function (req, reply, next) {
+        debug(`Getting sheets for whatever user`);
+        SheetsRepo.getAll()
+            .then(sheetInfo => {
+                if (!sheetInfo) {
+                    reply().code(404);
+                } else {
+                    reply(sheetInfo).code(200);
+                }
+            })
+            .catch(error => {
+                reply(error).code(500);
+            });
+    }
+};
