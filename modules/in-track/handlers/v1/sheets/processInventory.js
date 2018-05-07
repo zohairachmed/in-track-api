6d300a14edf0337c6e4475cd4ece887c03fb4534
Zohair Ahmed <zohair.ahmed@charter.com>
2018-05-07 12:21:31 -0600

    Added processingInventory for processing inventory.

---
+++ modules/in-track/handlers/v1/sheets/processInventory.js
@@ -0,0 +1,21 @@
+'use strict';
+const SheetsRepo = require('../../../../data-store/sheets-repo');
+const debug = require('debug')('sheets/list');
+
+module.exports = {
+    get: function (req, reply, next) {
+        debug(`Getting sheets for whatever user`);
+
+        SheetsRepo.getAllRows()
+        .then(sheetData => {
+            if (!sheetData) {
+                reply().code(404);
+            } else {
+                reply(sheetData).code(200);
+            }
+        })
+        .catch(error => {
+            reply(error).code(500);
+        });
+    }
+};
