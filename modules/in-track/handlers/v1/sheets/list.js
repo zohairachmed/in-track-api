651ee0458f4057bdb3af54257204430ba26dae87
Zohair Ahmed <zohair.ahmed@charter.com>
2018-05-07 12:20:41 -0600

    Added endpoint for processing inventory

--- modules/in-track/handlers/v1/sheets/list.js
+++ modules/in-track/handlers/v1/sheets/list.js
@@ -6,18 +6,6 @@ module.exports = {
     get: function (req, reply, next) {
         debug(`Getting sheets for whatever user`);
 
-        // SheetsRepo.getAllRows()
-        // .then(sheetData => {
-        //     if (!sheetData) {
-        //         reply().code(404);
-        //     } else {
-        //         reply(sheetData).code(200);
-        //     }
-        // })
-        // .catch(error => {
-        //     reply(error).code(500);
-        // });
-
         SheetsRepo.getAll()
             .then(sheetInfo => {
                 if (!sheetInfo) {
